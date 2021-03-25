3be03138c9577132e6348fa9c268d3fa2840982e
sharmadotankit <sharma.ankit.kamal@gmail.com>
2021-03-25 15:06:38 +0530

    pom changed

--- first-maven-app/src/main/java/com/lti/view/EmpView.java
+++ first-maven-app/src/main/java/com/lti/view/EmpView.java
@@ -6,7 +6,7 @@ class EmpView {
 	public static void main(String []args) {
 		Employee emp = new Employee();
 		emp.setEmpId(1002);
-		emp.setEmpName("Ankit");
+		emp.setEmpName("Modoriya");
 		System.out.println("Id : "+ emp.getEmpId());
 		System.out.println("Name : "+ emp.getEmpName());
 	}
