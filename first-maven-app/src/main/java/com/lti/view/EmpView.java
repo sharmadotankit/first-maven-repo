ce9b7054cf138395df14ffe353623563ec540335
sharmadotankit <sharma.ankit.kamal@gmail.com>
2021-03-25 15:08:44 +0530

    name

--- first-maven-app/src/main/java/com/lti/view/EmpView.java
+++ first-maven-app/src/main/java/com/lti/view/EmpView.java
@@ -6,7 +6,7 @@ class EmpView {
 	public static void main(String []args) {
 		Employee emp = new Employee();
 		emp.setEmpId(1002);
-		emp.setEmpName("Modoriya");
+		emp.setEmpName("Ankit");
 		System.out.println("Id : "+ emp.getEmpId());
 		System.out.println("Name : "+ emp.getEmpName());
 	}
